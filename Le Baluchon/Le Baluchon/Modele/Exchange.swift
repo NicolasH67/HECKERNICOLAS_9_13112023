3b937b6eac6f8b69b365397039f75120136180a5
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2023-11-21 09:30:26 +0100

    add Exchange.swift and Extension.swift

---
+++ Le Baluchon/Le Baluchon/Modele/Exchange.swift
@@ -0,0 +1,8 @@
+//
+//  Exchange.swift
+//  Le Baluchon
+//
+//  Created by Nicolas Hecker on 16/11/2023.
+//
+
+import Foundation
---
+++ Le Baluchon/Le Baluchon/Other Files/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  Le Baluchon
+//
+//  Created by Nicolas Hecker on 14/11/2023.
+//
+
+import Foundation
